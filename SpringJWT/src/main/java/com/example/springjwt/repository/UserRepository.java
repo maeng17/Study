76bc5f93088007e5fab8aecf3b7043da7ba8952d
maeng <soon5796735@naver.com>
2024-05-26 14:30:19 +0900

    [✨SpringJWT: 회원가입 로직 구현] * JoinDTO 생성 * JoinController 생성  - JoinService.joinProcess를 통해 회원가입 진행 * JoinService  - isExist로 같은 유저가 있는지 확인 후 없으면 데이터 담기 * UserRepository  - 같은 유저가 있는 지 찾는 함수 구현

--- SpringJWT/src/main/java/com/example/springjwt/repository/UserRepository.java
+++ SpringJWT/src/main/java/com/example/springjwt/repository/UserRepository.java
@@ -4,4 +4,6 @@ import com.example.springjwt.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<UserEntity, Integer> {
+
+    Boolean existsByUsername(String username);
 }
