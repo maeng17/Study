4232d06757ec9af1eb4bc0f24bc2308ff56ade07
maeng <soon5796735@naver.com>
2024-05-26 13:40:14 +0900

    [✨SpringJWT: SecurityConfig 및 application.properties 설정] * SecurityConfig  - 세션 설정  - BCryptPaasswordEncoder 등록 * application.properties  - DB 연결  - DDL 설정

---
+++ SpringJWT/src/main/java/com/example/springjwt/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.example.springjwt.repository;
+
+import com.example.springjwt.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<UserEntity, Integer> {
+}
