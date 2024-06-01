842b34a9e832bdcc9d088ec56db13b7977d1779d
maeng <soon5796735@naver.com>
2024-06-01 22:11:34 +0900

    [✨JPA 기본 : 프로젝션 ]

--- inflearn/JPA-start/jpql/src/main/java/jpql/Jpamain.java
+++ inflearn/JPA-start/jpql/src/main/java/jpql/Jpamain.java
@@ -16,6 +16,9 @@ public class Jpamain {
         member.setUsername("member1");
         em.persist(member);
 
+        em.flush();
+        em.clear();
+
 //        TypedQuery<Member> query1 = em.createQuery("SELECT m FROM Member m", Member.class);
 //        TypedQuery<String> query2 = em.createQuery("SELECT m.username FROM Member m", String.class); //하나의 타입 이용시 TypeQuery 사용가능
 //        Query query3 = em.createQuery("SELECT m.username, m.age FROM Member m"); //반환타입이 명확하지 않을 때
@@ -29,13 +32,56 @@ public class Jpamain {
 //        Member singleResult = query.getSingleResult();
 //        System.out.println("singleResult = " + singleResult.getUsername());
 
-        //위 보다 아래 코드가 더 good
-        Member result = em.createQuery("SELECT m FROM Member m where m.username = :username", Member.class)
-                        .setParameter("username", "member1")
-                        .getSingleResult();
-        System.out.println("result: " + result.getUsername());
+//        //위 보다 아래 코드가 더 good
+//        Member result = em.createQuery("SELECT m FROM Member m where m.username = :username", Member.class)
+//                        .setParameter("username", "member1")
+//                        .getSingleResult();
+//        System.out.println("result: " + result.getUsername());
 
-        em.flush();
+        //프로젝션
+//        List<Member> result = em.createQuery("SELECT m FROM Member m", Member.class)
+//                        .getResultList();
+//        Member findMember = result.get(0);
+//        findMember.setAge(20); // update -> 영속성 컨텍스트로 관리되어 데이터 반영됨
+
+        //엔티티 프로젝션
+//        List<Team> result = em.createQuery("SELECT m.team FROM Member m", Team.class) //join해서 쿼리 나감
+//                        .getResultList();
+
+        //임베디드 타입 프로젝션
+//        List<Address> result = em.createQuery("SELECT o.address FROM Order o", Address.class) //어디소속인디 엔티티를 정해줘야 함
+//                .getResultList();
+
+        //스칼라 타입 프로젝션
+//        List<Member> result = em.createQuery("select distinct m.username, m.age FROM Member m")
+//                .getResultList();
+
+
+        //여러값 조회
+        //1) 쿼리 타입으로 조회
+//        List<Member> resultList = em.createQuery("select distinct m.username, m.age FROM Member m")
+//                .getResultList();
+//
+//        Object o = resultList.get(0);
+//        Object[] result = (Object[]) o;
+//        System.out.println("result: " + result[0]);
+//        System.out.println("result: " + result[1]);
+
+        //Object[] 타입으로 조회
+//        List<Object[]> resultList = em.createQuery("select distinct m.username, m.age FROM Member m")
+//                .getResultList();
+//
+//        Object[] result = resultList.get(0);
+//        System.out.println("result: " + result[0]);
+//        System.out.println("result: " + result[1]);
+
+        //new 명령어로 조회
+        List<MemberDTO> result = em.createQuery("select new jpql.MemberDTO(m.username, m.age) FROM Member m", MemberDTO.class)
+                .getResultList();
+
+        MemberDTO memberDTO = result.get(0);
+        System.out.println("memberDTO: " + memberDTO.getUsername());
+        System.out.println("memberDTO: " + memberDTO.getAge());
         tx.commit();
         try {
         } catch (Exception e) {
