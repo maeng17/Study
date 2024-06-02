34e08b37a140295dd2aeb0dacf675004bf7cb8f0
maeng <soon5796735@naver.com>
2024-06-02 15:07:55 +0900

    [✨ SpringBoot & JPA 1: 프로젝트 생성] * 의존관계 설정 * View 환경설정 * test

---
+++ inflearn/SpringBoot&JPA_1/jpashop/src/main/java/jpabook/jpashop/HelloController.java
@@ -0,0 +1,15 @@
+package jpabook.jpashop;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("hello")
+    public String hello(Model model) {
+        model.addAttribute("data", "hello!!"); //name=data의 값 'hello!!'를 넘김
+        return "hello";
+    }
+}
