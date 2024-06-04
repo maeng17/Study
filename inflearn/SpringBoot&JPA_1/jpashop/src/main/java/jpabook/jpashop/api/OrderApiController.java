302d8b8f7b6a682a85ddcbcf4e4756b16f16a56b
maeng <soon5796735@naver.com>
2024-06-04 15:35:24 +0900

    [✨ JPA 2: 컬렉션 최적화 - 주문 조회] * 주문 조회 V1: 엔티티 직접 노출  - 지연로딩으로 인한 에러로 orderItems, orderItem 강제 초기화 * 주문 조회 V2: 엔티티를 DTO로 변환  - orderItems도 엔티티이기 때문에 dto로 변환하여 원하는 항목만 출력

---
+++ inflearn/SpringBoot&JPA_1/jpashop/src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -0,0 +1,95 @@
+package jpabook.jpashop.api;
+
+import jpabook.jpashop.domain.Address;
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderItem;
+import jpabook.jpashop.domain.OrderStatus;
+import jpabook.jpashop.repository.OrderRepository;
+import jpabook.jpashop.repository.OrderSearch;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.toList;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderApiController {
+
+    private final OrderRepository orderRepository;
+
+    //주문 조회 V1: 엔티티 직접 노출
+    @GetMapping("/api/v1/orders")
+    public List<Order> ordersV1() {
+        List<Order> all = orderRepository.findAllByString(new OrderSearch());
+        for (Order order : all) {
+            order.getMember().getName();
+            order.getDelivery().getAddress();
+
+            List<OrderItem> orderItems = order.getOrderItems(); // orderItems 강제초기화
+//            for(OrderItem orderItem : orderItems) {
+//                orderItem.getItem().getName(); // orderItem의 name 강제 초기화
+//            }
+            orderItems.stream().forEach(o -> o.getItem().getName());
+        }
+        return all;
+    }
+
+    //주문 조회 V2: 엔티티를 DTO로 변환 -> 컬렉션 사용 시 쿼리 더 많이 나감
+    @GetMapping("/api/v2/orders")
+    public List<OrderDto> ordersV2() {
+        List<Order> orders = orderRepository.findAllByString(new OrderSearch());
+        List <OrderDto> result = orders.stream()
+                .map(o -> new OrderDto(o))
+                .collect(toList());
+
+        return result;
+    }
+
+    @Getter
+    static class OrderDto {
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+        //private List<OrderItem> orderItems; //엔티티가 변하면 API 스펙이 변함 ->  dto변환 해야함
+        private List<OrderItemDto> orderItems;
+
+        public OrderDto(Order order) {
+            orderId = order.getId();
+            name = order.getMember().getName();
+            orderDate = order.getOrderDate();
+            orderStatus = order.getStatus();
+            address = order.getDelivery().getAddress();
+//            order.getOrderItems().stream().forEach(o -> o.getItem().getName()); //강제 초기화해야 orderItems 값 들어옴
+//            orderItems = order.getOrderItems(); //단독 사용시 null -> 엔티티이기 때문
+            orderItems = order.getOrderItems().stream()
+                    .map(orderItem -> new OrderItemDto(orderItem))
+                    .collect(toList());
+
+        }
+    }
+
+    @Getter
+    static class OrderItemDto{
+        private String itemName; //상품명
+        private int orderPrice; //주문 가격
+        private int count; //주문 수량
+
+        public OrderItemDto(OrderItem orderItem) {
+            itemName = orderItem.getItem().getName();
+            orderPrice = orderItem.getOrderPrice();
+            count = orderItem.getCount();
+        }
+
+    }
+
+
+    }
--- inflearn/SpringBoot&JPA_1/jpashop/src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ inflearn/SpringBoot&JPA_1/jpashop/src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -30,7 +30,7 @@ public class OrderSimpleApiController {
     private final OrderRepository orderRepository;
     private final OrderSimpleQueryRepository orderSimpleQueryRepository;
 
-    //엔티티 직접 노출 -> 지연로딩 성능 문제
+    //간단 주문 조회: 엔티티 직접 노출 -> 지연로딩 성능 문제
     @GetMapping("/api/v1/simple-orders")
     public List<Order> orderV1() {
         List<Order> all = orderRepository.findAllByString(new OrderSearch()); //무한루프 -> 양방향 연관관계 한쪽은 jsonignore
@@ -41,7 +41,7 @@ public class OrderSimpleApiController {
         return all;
     }
 
-    //엔티티를 DTO로 변환 - 지연로딩으로 인해 쿼리가 너무 많이 호출됨(1 + N + N)
+    //간단 주문 조회: 엔티티를 DTO로 변환 - 지연로딩으로 인해 쿼리가 너무 많이 호출됨(1 + N + N)
     @GetMapping("/api/v2/simple-orders")
     public List<SimpleOrderDto> orderV2() {
         //ORDER 2개
@@ -56,7 +56,7 @@ public class OrderSimpleApiController {
         return result;
     }
 
-    //fetch join 사용 (엔티티를 DTO로 변환) : 쿼리 한번만 호출
+    //간단 주문 조회: fetch join 사용 (엔티티를 DTO로 변환) : 쿼리 한번만 호출
     // 외부의 모습을 건드리지 않고 내부에서 원하는 것만 패치 조인으로 추출 -> 재사용성 높음
     // -> select시 엔티티를 찍어서 줘야함. 셀렉트절의 양이 많음
     // 비즈니스 로직 사용 -> 데이터 변경 가능
@@ -71,7 +71,7 @@ public class OrderSimpleApiController {
         return result;
     }
 
-    //JPA에서 DTO로 바로 조회: select 절의 양이 감소 -> 성능 최적화
+    //간단 주문 조회: JPA에서 DTO로 바로 조회: select 절의 양이 감소 -> 성능 최적화
     //JPQL 직접 작성 -> 리파지토리 재사용성 떨어짐
     // DTO로 조회 -> 데이터 변경 불가능(엔티티가 아니기 때문)
     @GetMapping("/api/v4/simple-orders")
